c4c444a2d390940a38e691cdd1ac7a9f6a174a12
toilaluan <toilaluan@github.com>
2024-01-11 01:29:05 +0700

    finish data flow, task: visualization

--- cassandra/setup.sh
+++ cassandra/setup.sh
@@ -16,3 +16,11 @@ echo "USE $KEYSPACE;" | cqlsh $CASSANDRA_HOST $CASSANDRA_PORT
 
 # Create table
 echo "CREATE TABLE IF NOT EXISTS $KEYSPACE.$TABLE (id UUID PRIMARY KEY, Season text, DateTime text, HomeTeam text, AwayTeam text, FTHG int, FTAG int, FTR text, HTHG int, HTAG int, HTR text, Referee text, HS int, AS int, HST int, AST int, HC int, AC int, HF int, AF int, HY int, AY int, HR int, AR int);" | cqlsh $CASSANDRA_HOST $CASSANDRA_PORT
+
+if [ $? -eq 0 ]
+then
+  echo "Cassandra setup successfully."
+else
+  echo "Cassandra setup failed."
+  exit 1
+fi
